2004607dfaf6e0786eb7a5226a8f16eea5a48874
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-07-20 22:20:47 -0300

    REST Parameters and Exception Handler

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,46 @@
+package br.com.erudio.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.erudio.exceptions.ExceptionResponse;
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+
+@ControllerAdvice // Diz ao Spring que esse é um controller especial e que potencialmente todos os outros controllers irão usar recursos dele
+@RestController  // Diz ao Spring que esse é um controller 
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	// Intercepta qualquer exceção de modo genérico e trata de forma elegante
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+		
+		// Substitui aquela excessão feia por um JSON
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		// Retorna uma response com um status code genérico
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	// Intercepta as exceções que nós personalizamos
+	@ExceptionHandler(UnsupportedMathOperationException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+		
+		// Substitui aquela excessão feia por um JSON
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		// Retorna uma response com um status code de BAD_REQUEST (podemos usar qualquer status code)
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+}
