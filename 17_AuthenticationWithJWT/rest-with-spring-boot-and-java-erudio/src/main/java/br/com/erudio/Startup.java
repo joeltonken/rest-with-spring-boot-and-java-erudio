db78a2f92db67be0f00d12268bb6eab63af2dab0
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-12-14 23:46:46 -0300

    test token jwt

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -1,13 +1,34 @@
 package br.com.erudio;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class Startup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
+
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+		encoders.put("pbkdf2", new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
+
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+
+		passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
+
+
+
+		String result = passwordEncoder.encode("admin123");
+
+		System.out.println("My hash " + result);
 	}
 
 }
--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
@@ -8,6 +8,6 @@ import br.com.erudio.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	
-	@Query("SELECT u FROM User WHERE u.userName =:userName")
+	@Query("SELECT u FROM User u WHERE u.userName =:userName")
 	User findByUsername(@Param("userName") String userName);
 }
