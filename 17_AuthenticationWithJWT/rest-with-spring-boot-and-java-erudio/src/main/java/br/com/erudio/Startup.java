4710e1d389426866c4d375a06d0e1b269fa45c93
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-12-22 20:35:36 -0300

    finishing jwt

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -1,13 +1,13 @@
 package br.com.erudio;
 
-import java.util.HashMap;
-import java.util.Map;
+/*import java.util.HashMap;
+import java.util.Map;*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+/*import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;*/
 
 @SpringBootApplication
 public class Startup {
@@ -15,9 +15,9 @@ public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 
+		/*Map<String, PasswordEncoder> encoders = new HashMap<>();
+		
         Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
 		encoders.put("pbkdf2", pbkdf2Encoder);
 		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
 		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
@@ -26,7 +26,7 @@ public class Startup {
 		String result2 = passwordEncoder.encode("admin234");
 
 		System.out.println("My hash " + result);
-		System.out.println("My hash result 2 " + result2);
+		System.out.println("My hash result 2 " + result2);*/
 	}
 
 }
