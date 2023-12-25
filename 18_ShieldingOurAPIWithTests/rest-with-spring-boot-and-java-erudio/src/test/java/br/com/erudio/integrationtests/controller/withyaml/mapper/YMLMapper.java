654a87a8802188f4e688fedf1f0d0bbe55adf452
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-12-25 18:32:27 -0300

    add log

--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controller/withyaml/mapper/YMLMapper.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controller/withyaml/mapper/YMLMapper.java
@@ -1,5 +1,7 @@
 package br.com.erudio.integrationtests.controller.withyaml.mapper;
 
+import java.util.logging.Logger;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -11,6 +13,8 @@ import io.restassured.mapper.ObjectMapperDeserializationContext;
 import io.restassured.mapper.ObjectMapperSerializationContext;
 
 public class YMLMapper implements ObjectMapper{
+	
+	private Logger logger = Logger.getLogger(YMLMapper.class.getName());
 	
 	private com.fasterxml.jackson.databind.ObjectMapper objectMapper;
 	protected TypeFactory typeFactory;
@@ -28,10 +32,14 @@ public class YMLMapper implements ObjectMapper{
 			String dataToDeserialize = context.getDataToDeserialize().asString();
 			Class type = (Class)context.getType();
 			
+			logger.info("Trying deserialize object of type" + type);
+			
 			return objectMapper.readValue(dataToDeserialize, typeFactory.constructType(type));
 		} catch (JsonMappingException e) {
+            logger.severe("Error deserializing object");
 			e.printStackTrace();
 		} catch (JsonProcessingException e) {
+            logger.severe("Error deserializing object");
 			e.printStackTrace();
 		}
 		return null;
