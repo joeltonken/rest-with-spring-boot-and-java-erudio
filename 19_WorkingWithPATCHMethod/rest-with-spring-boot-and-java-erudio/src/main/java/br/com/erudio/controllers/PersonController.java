8e9a1fb8f1999009ad19858d973cc7e90fa975ac
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-12-29 19:21:15 -0300

    adding method with PATCH in controller

--- 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -112,6 +113,25 @@ public class PersonController {
 	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
+	
+	@PatchMapping(value = "/{id}",
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
+		@Operation(summary = "Disable a specific Person by your ID", description = "Disable a specific Person by your ID",
+			tags = {"People"},
+			responses = {
+				@ApiResponse(description = "Success", responseCode = "200",
+					content = @Content(schema = @Schema(implementation = PersonVO.class))
+				),
+				@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			}
+		)
+		public PersonVO disablePerson(@PathVariable(value = "id") Long id) {
+			return service.disablePerson(id);
+		}
 
 	@DeleteMapping(value = "{id}")
 	@Operation(summary = "Deletes a Person",
