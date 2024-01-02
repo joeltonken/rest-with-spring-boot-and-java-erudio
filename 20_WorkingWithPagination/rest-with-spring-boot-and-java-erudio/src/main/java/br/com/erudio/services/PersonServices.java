c8ef22f6fe07a95a6cbfd2735fb62fd39338fede
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-01-02 20:36:56 -0300

    passing parameters to pageable

--- 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -35,7 +35,7 @@ public class PersonServices {
 	
 	public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
 		
-		logger.info("Finding all people!");
+		logger.info("Finds People by Name!");
 		
 		var personPage = repository.findAll(pageable);
 		var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
@@ -46,6 +46,27 @@ public class PersonServices {
 
 		Link link = linkTo(
 				methodOn(PersonController.class).findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc")).withSelfRel();
+		return assembler.toModel(personVosPage, link);
+	}
+	
+	public PagedModel<EntityModel<PersonVO>> findPersonByName(String firstname, Pageable pageable) {
+		
+		logger.info("Finding all people!");
+		
+		var personPage = repository.findPersonsByName(firstname, pageable);
+		
+		var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+		personVosPage.map(
+				p -> p.add(
+						linkTo(methodOn(PersonController.class)
+								.findById(p.getKey())).withSelfRel()));
+		
+		Link link = linkTo(
+				methodOn(PersonController.class)
+				.findAll(pageable.getPageNumber(),
+						pageable.getPageSize(),
+						"asc")).withSelfRel();
+		
 		return assembler.toModel(personVosPage, link);
 	}
 
